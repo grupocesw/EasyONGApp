c236a299a03731434ef800036dab172280fad05c
Nicholas Santos <nicholas.santos.stefanini@elo.com.br>
2021-05-12 13:08:18 -0300

    fix: remove var not-unsed

--- src/Contexts/UsersContext.tsx
+++ src/Contexts/UsersContext.tsx
@@ -9,8 +9,6 @@ export const UsersContext = createContext({});
 
 export const UsersProvider = ({children}: any) => {
   const [User, setUser] = useState(UsersData);
-  const varToken =
-    'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhbmRyZXJpZ2dzOTBAZ21haWwuY29tIiwiYXV0aG9yaXRpZXMiOlsiVVNFUiJdLCJpYXQiOjE2MjA2NzU3MDksImV4cCI6MTYyMDc2MjEwOX0.LfqXtDtECwt9pZgjX9OdICvbzfdM-w_IJmdRbSlVvuNrkIrEx1JJexP1mOxzL8E6M8TBwLhglLvXsMZ5NjWEKg';
   const [Token, setToken] = useState('');
   return (
     <UsersContext.Provider
