e4940e8b083154c0a58fff4c735a75ff35e39bd6
Nicholas Santos <nicholas.santos.stefanini@elo.com.br>
2021-05-13 15:31:56 -0300

    feat: add screenShot share

--- android/app/src/main/java/br/com/easyongapp/MainApplication.java
+++ android/app/src/main/java/br/com/easyongapp/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import fr.greweb.reactnativeviewshot.RNViewShotPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
